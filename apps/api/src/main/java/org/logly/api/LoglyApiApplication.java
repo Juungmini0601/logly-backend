6d24d812d6421a08a171b9b5843b19dfd0374463
jungmin <jungmini0601@gmail.com>
2025-09-15 16:01:13 +0900

    add README.md 재시작

--- apps/api/src/main/java/org/logly/api/LoglyApiApplication.java
+++ apps/api/src/main/java/org/logly/api/LoglyApiApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LoglyApiApplication {
 
     public static void main(String[] args) {
-            SpringApplication.run(LoglyApiApplication.class, args);
+        SpringApplication.run(LoglyApiApplication.class, args);
     }
 }
